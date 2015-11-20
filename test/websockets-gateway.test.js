ed33869674c9bfed3f26aac322c53e54ac2bb4fd
Benj Sicam <bsicam@reekoh.com>
2015-11-20 16:28:29 +0800

    REEKOH-249 Gateway Plugin - Vanilla Websockets Gateway
    Initial Commit

---
+++ test/websockets-gateway.test.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const PORT       = 8080,
+	  CLIENT_ID1 = '567827489028375',
+	  CLIENT_ID2 = '567827489028376';
+
+var cp        = require('child_process'),
+	should    = require('should'),
+	WebSocket = require('ws'),
+	wsGateway;
+
+describe('WS Gateway', function () {
+	this.slow(8000);
+
+	after('terminate child process', function () {
+		this.timeout(5000);
+
+		wsGateway.send({
+			type: 'close'
+		});
+
+		setTimeout(function () {
+			wsGateway.kill('SIGKILL');
+		}, 4500);
+	});
+
+	describe('#spawn', function () {
+		it('should spawn a child process', function () {
+			should.ok(wsGateway = cp.fork(process.cwd()), 'Child process not spawned.');
+		});
+	});
+
+	describe('#handShake', function () {
+		it('should notify the parent process when ready within 8 seconds', function (done) {
+			this.timeout(8000);
+
+			wsGateway.on('message', function (message) {
+				if (message.type === 'ready')
+					done();
+			});
+
+			wsGateway.send({
+				type: 'ready',
+				data: {
+					options: {
+						port: PORT
+					},
+					devices: [{_id: CLIENT_ID1}, {_id: CLIENT_ID2}]
+				}
+			}, function (error) {
+				should.ifError(error);
+			});
+		});
+	});
+
+	describe('#message', function () {
+		it('should be able to process data', function (done) {
+			this.timeout(5000);
+
+			var url = 'http://127.0.0.1:' + PORT;
+			var ws = new WebSocket(url);
+
+			ws.on('open', function () {
+				ws.send(JSON.stringify({
+					type: 'data',
+					device: '567827489028375',
+					co2: '11%',
+					o2: '20%'
+				}));
+			});
+
+			setTimeout(function () {
+				done();
+			}, 2000);
+		});
+	});
+});
